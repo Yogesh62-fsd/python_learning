558f0393fc95eb961320f01d262810e4af4088be
Yogesh Pandey <yogesh00364@gmail.com>
2023-01-07 16:11:33 +0530

    [ADD] Adding new file

---
+++ calculator.py
@@ -0,0 +1,8 @@
+a=20
+b=4
+print('add of a+b is ',a+b)
+print('sub of a-b is ',a-b)
+print('mul of a*b is ',a*b)
+print('div of a/b is ',a/b)
+print('mod of a%b is ',a%b)
+print('floor division of a//b is ',a//b)
